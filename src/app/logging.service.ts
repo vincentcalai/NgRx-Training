86429af8a353f50274142fd0366853857d1862b9
Vincent Lai <vincentchoonann.lai@dxc.com>
2022-06-13 20:56:03 +0800

    fixed app not launching due to logging service did not inject providedIn: root

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from "@angular/core";
-import { inject } from "@angular/core/testing";
+import { Injectable } from '@angular/core';
+import { inject } from '@angular/core/testing';
 
-//@Injectable({providedIn: 'root'})
-export class LoggingService{
-    lastlog: string;
+@Injectable({ providedIn: 'root' })
+export class LoggingService {
+  lastlog: string;
 
-    printLog(message: string){
-        console.log(message);
-        console.log(this.lastlog);
-        this.lastlog = message;
-    }
+  printLog(message: string) {
+    console.log(message);
+    console.log(this.lastlog);
+    this.lastlog = message;
+  }
 }
